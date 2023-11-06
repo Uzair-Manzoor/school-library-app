9557bc8a016802b651799171a92113ed3853b0ae
Uzair-Manzoor <uzair.kayani555@gmail.com>
2023-11-06 16:00:33 +0500

    add teacher class

--- student.rb
+++ student.rb
@@ -13,10 +13,10 @@ class Student < Person
   end
 end
 
-student1 = Student.new(0001, 15, 'Adham', parent_permission: false, classroom: 'Computer science')
-student2 = Student.new(0002, 17, 'Najlae', classroom: 'Applied physics')
+student1 = Student.new(0o001, 15, 'AbdulAziz', parent_permission: false, classroom: 'Computer science')
+student2 = Student.new(0o002, 17, 'AbdulSami', classroom: 'Applied physics')
 
-puts student1.name # Adham
+puts student1.name # AbdulAziz
 puts student1.classroom # Computer science
 puts student1.can_use_services? # false
 
