ede72371d6d347ab94c92791c355a06fb4d87951
Uzair-Manzoor <uzair.kayani555@gmail.com>
2023-11-08 19:17:42 +0500

    implement suggested changes

--- person.rb
+++ person.rb
@@ -1,10 +1,11 @@
 require './nameable'
 
-class Person
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
   def initialize(age, name: 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -16,6 +17,8 @@ class Person
   def of_age?
     @age >= 18
   end
+
+  public
 
   def can_use_services?
     of_age? || @parent_permission
