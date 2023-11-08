957e0991701061f12226b8c01712278d5612246c
Uzair-Manzoor <uzair.kayani555@gmail.com>
2023-11-08 23:57:27 +0500

    create relationship between Person and Book using intermediate class Rental

--- person.rb
+++ person.rb
@@ -1,15 +1,16 @@
 require './nameable'
 
 class Person < Nameable
-  attr_reader :id
+  attr_reader :id, :rentals
   attr_accessor :name, :age
 
-  def initialize(age, name: 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   private
@@ -18,11 +19,17 @@ class Person < Nameable
     @age >= 18
   end
 
+  public
+
   def can_use_services?
     of_age? || @parent_permission
   end
 
   def correct_name
     @name
+  end
+
+  def add_rental(rental)
+    @rentals << rental
   end
 end
