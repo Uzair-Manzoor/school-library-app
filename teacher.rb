f181a9e51f9610eb131c21537e0f7146307f7929
Uzair-Manzoor <uzair.kayani555@gmail.com>
2023-11-15 21:43:09 +0500

    update create teacher functionality

--- teacher.rb
+++ teacher.rb
@@ -3,12 +3,12 @@ require_relative 'person'
 class Teacher < Person
   attr_accessor :specialization
 
-  def initialize(age, specialization, name: 'Unknown', parent_permission: true)
-    super(age, name: name, parent_permission: parent_permission)
+  def initialize(age, specialization, name = 'Unknown')
+    super(name, age)
     @specialization = specialization
   end
 
-  def can_use_services?
+  def can_use_services
     true
   end
 end
