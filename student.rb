40631d4ad714cb5bca02bd272db19a4e8b47be2d
Enoh Collins <eno.col@gmail.com>
2023-11-21 08:58:06 +0000

    Edit student.rb

--- student.rb
+++ student.rb
@@ -1,6 +1,8 @@
 require './person'
 
 class Student < Person
+  attr_reader :classroom
+
   def initialize(age, name = 'Unknown', parent_permission: true, classroom: 'Unknown')
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
@@ -8,6 +10,10 @@ class Student < Person
 
   def play_hooky
     '¯\(ツ)/¯'
+  end
+
+  def display_info
+    "[Student] name: #{name}, ID: #{id}, Age: #{age}"
   end
 
   def classroom=(classroom)
