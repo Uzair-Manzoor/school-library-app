c3ceda284541514fd880e4e59ebc257a96c75f70
Enoh Collins <eno.col@gmail.com>
2023-11-21 23:29:07 +0000

    Edit app.rb

--- app.rb
+++ app.rb
@@ -1,8 +1,17 @@
 require './people_manager'
 require './books_manager'
 require './rentals_manager'
+require './person_factory'
+require './book_factory'
+require './rental_factory'
+require './serialization'
 
 class App
+  include PersonFactory
+  include BookFactory
+  include RentalFactory
+  include Serializition
+
   attr_accessor :people_manager, :books_manager, :rentals_manager
 
   def initialize
@@ -11,13 +20,15 @@ class App
     @rentals_manager = RentalsManager.new(@books_manager, @people_manager)
   end
 
-  def serialize_to_json(data, filename)
-    File.open(filename, 'w') { |file| file.puts JSON.generate(data) }
+  def save_data
+    save_books
+    save_people
+    save_rentals
   end
 
-  def deserialize_from_json(filename)
-    return [] unless File.exist?(filename)
-
-    JSON.parse(File.read(filename))
+  def load_data
+    load_books
+    load_people
+    load_rentals
   end
 end
